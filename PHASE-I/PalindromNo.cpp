468115422d613d8287c4c602a4c10d50f8aa12fd
SaurabhKarki-25 <saurabhsinghkarkisaurabh10@gmail.com>
2026-08-08 12:32:23 +0530

    Update

--- PHASE-I/PalindromNo.cpp
+++ PHASE-I/PalindromNo.cpp
@@ -4,34 +4,32 @@
 
 using namespace std;
 
+void palindrome(int n){
+    int original = n;
+    int rem , ans = 0 ;
+
+    while(n > 0){
+        rem = n % 10;
+        n = n / 10;
+        ans = ans * 10 + rem ;
+    }
+    
+    if(original == ans){
+          cout << original << " == " << ans << endl <<  "Number is Palindrome " << endl;
+    }else{
+        cout << original << " != " << ans << endl << "Number is not a Palindrome " << endl;
+        
+    }
+
+}
+
 int main()
 {   
     int n;
     cout << "Enter the No to check: " << endl ;
     cin  >> n;
-
-    int original = n;
-    int rem , ans = 0;
-    if(n >= 0 && n <= 1000000000){
-
-        while(n > 0){
-            rem = n % 10 ; 
-            n = n / 10 ;
-            ans = ans * 10 + rem;
-        }
-
-      
-    }else{
-        return -1;
-    }
-
     
-
-    if(original == ans){
-          cout << original << " == " << ans << endl <<  "Number is Palindrome " << endl;
-    }else{
-        cout << original << " != " << ans << endl << "Number is not a Palindrome " << endl;
-    }
+    palindrome(n);
 
     return 0;
 }
